8b9c484810650a2c7abd8506bd887502e9fe2e59
Joonha Yi <JoonhaYi@gmail.com>
2025-10-22 19:06:22 +0900

    updated javascript

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+// inserting nabar html to the page header
 fetch("navbar.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("navbar_div").innerHTML = data;
+
+    // adding listeners to the sidebar button one the navbar loads
     addListeners();
   });
 
 function addListeners() {
+  // extracting DOM
   const button = document.getElementById("toggle_sidebar");
   const sidebar = document.getElementById("navbar");
 
   button.addEventListener("click", () => {
+    // toggling on css class open
     sidebar.classList.toggle("open");
     button.classList.toggle("open");
   });
 
+  // closing the sidebar if an arbitrary click is detected
   document.addEventListener("click", function (event) {
-    // alert(sidebar.classList.contains("open"));
-    if (!button.contains(event.target)) {
+    // checking if the sidebar is already open
+    if (!button.contains(event.target) && sidebar.classList.contains("open")) {
       sidebar.classList.toggle("open");
       button.classList.toggle("open");
     }
